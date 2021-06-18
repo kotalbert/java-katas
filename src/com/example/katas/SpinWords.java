84557d9ff6fdf76aa4db527c723e5645cdff7b96
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-18 21:07:48 +0200

    Add methods wishlist

--- src/com/example/katas/SpinWords.java
+++ src/com/example/katas/SpinWords.java
@@ -4,7 +4,22 @@ public class SpinWords {
 
     public String spinWords(String sentence) {
         //TODO: Code stuff here
+        var words = sentence.split(" ");
 
-        return "";
+        for (int i=0;i<words.length;i++) {
+           if (words[i].length() > 5) {
+               words[i] = reverse(words[i]);
+           }
+        }
+
+        return collect(words);
+    }
+
+    private String collect(String[] words) {
+        return null;
+    }
+
+    private String reverse(String word) {
+        return null;
     }
 }
