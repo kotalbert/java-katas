9eaa0225b33bae219fe97edb7a4763cdbdb2e5c4
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-18 21:14:01 +0200

    Implement SpinWords with StringBuilder

--- src/com/example/katas/SpinWords.java
+++ src/com/example/katas/SpinWords.java
@@ -3,23 +3,22 @@ package com.example.katas;
 public class SpinWords {
 
     public String spinWords(String sentence) {
-        //TODO: Code stuff here
         var words = sentence.split(" ");
 
-        for (int i=0;i<words.length;i++) {
-           if (words[i].length() > 5) {
-               words[i] = reverse(words[i]);
-           }
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() >= 5) {
+                words[i] = reverse(words[i]);
+            }
         }
 
         return collect(words);
     }
 
     private String collect(String[] words) {
-        return null;
+        return String.join(" ", words);
     }
 
     private String reverse(String word) {
-        return null;
+        return new StringBuilder(word).reverse().toString();
     }
 }
