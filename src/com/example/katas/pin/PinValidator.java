40446ceaf99e0ad89062a232c73e5d7f1aac232b
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-18 23:38:55 +0200

    Solve PinValidator kata

--- src/com/example/katas/pin/PinValidator.java
+++ src/com/example/katas/pin/PinValidator.java
@@ -2,7 +2,18 @@ package com.example.katas.pin;
 
 public class PinValidator {
     public static boolean validatePin(String pin) {
-        // Your code here...
+        return allDigits(pin) && rightLength(pin);
+    }
+
+    private static boolean rightLength(String pin) {
+        return pin.length() == 4 || pin.length() == 6;
+    }
+
+    private static boolean allDigits(String pin) {
+        for (char c : pin.toCharArray()) {
+            if  (!Character.isDigit(c)) return false;
+        }
         return true;
     }
+
 }
